fc14add3652df772a0823108dbfce63a483624bf
blurp <38859310+blurpsk@users.noreply.github.com>
2021-11-25 11:59:51 +0000

    LOL

--- AutoClicker/UI/UI.cpp
+++ AutoClicker/UI/UI.cpp
@@ -6,20 +6,7 @@
 
 #include <algorithm>
 
-// Data
-static LPDIRECT3D9              g_pD3D = NULL;
-static LPDIRECT3DDEVICE9        g_pd3dDevice = NULL;
-static D3DPRESENT_PARAMETERS    g_d3dpp = {};
-
-// Forward declarations of helper functions
-bool CreateDeviceD3D(HWND hWnd);
-void CleanupDeviceD3D();
-void ResetDevice();
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
 LPDIRECT3DTEXTURE9 logo = nullptr;
-
-LPDIRECT3DTEXTURE9 icons[3] = {nullptr, nullptr, nullptr};
 
 void build_textures()
 {
@@ -27,193 +14,7 @@ void build_textures()
     {
         D3DXCreateTextureFromFileInMemory(g_pd3dDevice, &raw_logo, sizeof(raw_logo), &logo);
     }
-
-    if (icons[0] == nullptr)
-    {
-        D3DXCreateTextureFromFileInMemory(g_pd3dDevice, &raw_cursor, sizeof(raw_cursor), &icons[0]);
-    }
-
-    if (icons[1] == nullptr)
-    {
-        D3DXCreateTextureFromFileInMemory(g_pd3dDevice, &raw_skull, sizeof(raw_skull), &icons[1]);
-    }
-
-    if (icons[2] == nullptr)
-    {
-        D3DXCreateTextureFromFileInMemory(g_pd3dDevice, &raw_settings, sizeof(raw_settings), &icons[2]);
-    }
 }
-
-inline bool IsKeyPressedMap(ImGuiKey key, bool repeat = true) { const int key_index = GImGui->IO.KeyMap[key]; return (key_index >= 0) ? ImGui::IsKeyPressed(key_index, repeat) : false; }
-
-const char* const KeyNames[] = {
-    "Unknown",
-    "VK_LBUTTON",
-    "VK_RBUTTON",
-    "VK_CANCEL",
-    "VK_MBUTTON",
-    "VK_XBUTTON1",
-    "VK_XBUTTON2",
-    "Unknown",
-    "VK_BACK",
-    "VK_TAB",
-    "Unknown",
-    "Unknown",
-    "VK_CLEAR",
-    "VK_RETURN",
-    "Unknown",
-    "Unknown",
-    "VK_SHIFT",
-    "VK_CONTROL",
-    "VK_MENU",
-    "VK_PAUSE",
-    "VK_CAPITAL",
-    "VK_KANA",
-    "Unknown",
-    "VK_JUNJA",
-    "VK_FINAL",
-    "VK_KANJI",
-    "Unknown",
-    "VK_ESCAPE",
-    "VK_CONVERT",
-    "VK_NONCONVERT",
-    "VK_ACCEPT",
-    "VK_MODECHANGE",
-    "VK_SPACE",
-    "VK_PRIOR",
-    "VK_NEXT",
-    "VK_END",
-    "VK_HOME",
-    "VK_LEFT",
-    "VK_UP",
-    "VK_RIGHT",
-    "VK_DOWN",
-    "VK_SELECT",
-    "VK_PRINT",
-    "VK_EXECUTE",
-    "VK_SNAPSHOT",
-    "VK_INSERT",
-    "VK_DELETE",
-    "VK_HELP",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "VK_LWIN",
-    "VK_RWIN",
-    "VK_APPS",
-    "Unknown",
-    "VK_SLEEP",
-    "VK_NUMPAD0",
-    "VK_NUMPAD1",
-    "VK_NUMPAD2",
-    "VK_NUMPAD3",
-    "VK_NUMPAD4",
-    "VK_NUMPAD5",
-    "VK_NUMPAD6",
-    "VK_NUMPAD7",
-    "VK_NUMPAD8",
-    "VK_NUMPAD9",
-    "VK_MULTIPLY",
-    "VK_ADD",
-    "VK_SEPARATOR",
-    "VK_SUBTRACT",
-    "VK_DECIMAL",
-    "VK_DIVIDE",
-    "VK_F1",
-    "VK_F2",
-    "VK_F3",
-    "VK_F4",
-    "VK_F5",
-    "VK_F6",
-    "VK_F7",
-    "VK_F8",
-    "VK_F9",
-    "VK_F10",
-    "VK_F11",
-    "VK_F12",
-    "VK_F13",
-    "VK_F14",
-    "VK_F15",
-    "VK_F16",
-    "VK_F17",
-    "VK_F18",
-    "VK_F19",
-    "VK_F20",
-    "VK_F21",
-    "VK_F22",
-    "VK_F23",
-    "VK_F24",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "VK_NUMLOCK",
-    "VK_SCROLL",
-    "VK_OEM_NEC_EQUAL",
-    "VK_OEM_FJ_MASSHOU",
-    "VK_OEM_FJ_TOUROKU",
-    "VK_OEM_FJ_LOYA",
-    "VK_OEM_FJ_ROYA",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "Unknown",
-    "VK_LSHIFT",
-    "VK_RSHIFT",
-    "VK_LCONTROL",
-    "VK_RCONTROL",
-    "VK_LMENU",
-    "VK_RMENU"
-};
 
 bool CustomGui::Hotkey(const char* label, int* k, const ImVec2& size_arg)
 {
@@ -403,30 +204,15 @@ int main(int, char**)
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplDX9_Init(g_pd3dDevice);
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 0.00f);
-    bool _winsz = false;
 
     build_textures();
-
     SetupStyle();
 
     const char* tabs[] = { "CLICKER", "MISC", "CONFIG" };
     static int iSelectedTab = 0;
+
+    ImGui::SetNextWindowSize(ImVec2(800, 400));
 
     // Main loop
     bool done = false;
@@ -435,11 +221,6 @@ int main(int, char**)
 
         AutoClicker::HandleClicks();
 
-        // Poll and handle messages (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         MSG msg;
         while (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
         {
@@ -455,21 +236,10 @@ int main(int, char**)
         ImGui_ImplDX9_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
-
-        if (!_winsz) 
+    
         {
-            ImGui::SetNextWindowSize(ImVec2(800, 400));
-            _winsz = true;
-        }
-               
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            bool bOpen = true;
 
-            /* if (GetForegroundWindow() == hWND) {*/
-
-            ImGui::Begin("Hello, world!", &bOpen, ImGuiWindowFlags_NoTitleBar);                          // Create a window called "Hello, world!" and append into it.
-                
+            ImGui::Begin("Hello, world!", nullptr, ImGuiWindowFlags_NoTitleBar);
             {
                 ImGui::GetWindowDrawList()->AddRectFilledMultiColor(ImVec2(ImGui::GetWindowPos().x + 100, ImGui::GetWindowPos().y), ImVec2(ImGui::GetWindowPos().x + 120, ImGui::GetWindowPos().y + 400), ImColor(0.f, 0.f, 0.f, .2f), ImColor(0.f, 0.f, 0.f, 0.f), ImColor(0.f, 0.f, 0.f, 0.f), ImColor(0.f, 0.f, 0.f, .2f));
             }
@@ -477,8 +247,6 @@ int main(int, char**)
             ImGui::BeginChild("##navbar", ImVec2(100, 380), false);
             ImGui::SetCursorPos(ImVec2(22.5f, 22.5f));
             ImGui::Image(logo, ImVec2(50, 50));
-
-            // ImVec4(12 / 255.f, 25 / 255.f, 47 / 255.f, 1.f);
 
             {
                 for (int i = 0; i < _ARRAYSIZE(tabs); i++)
@@ -503,7 +271,7 @@ int main(int, char**)
 
 
             ImGui::End();
-            /* }*/
+  
         }
 
         // Rendering
@@ -542,7 +310,6 @@ int main(int, char**)
     return 0;
 }
 
-// Helper functions
 
 bool CreateDeviceD3D(HWND hWnd)
 {
@@ -557,7 +324,6 @@ bool CreateDeviceD3D(HWND hWnd)
     g_d3dpp.EnableAutoDepthStencil = TRUE;
     g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
     g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;           // Present with vsync
-    //g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present without vsync, maximum unthrottled framerate
     if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
         return false;
 
